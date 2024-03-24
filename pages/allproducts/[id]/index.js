e84a44865e69ecc4d16f1faa3a0455eb8e4ed6fe
Sovan Roy <roysovan00@gmail.com>
2024-03-25 01:26:57 +0530

    fixing add to cart button

--- pages/allproducts/[id]/index.js
+++ pages/allproducts/[id]/index.js
@@ -278,7 +278,7 @@ export default function ProductDetails({ product }) {
                     product.title,
                     product.description,
                     product.price,
-                    product.imageUrl[0]
+                    product.images[0]
                   )
                 }
                 className="mt-10 flex w-full max-w-[200px] items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 "
