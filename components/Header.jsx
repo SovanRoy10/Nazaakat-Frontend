d59c5d8e82327bbd7dbf2d4816888f9d925567a0
Sovan Roy <roysovan00@gmail.com>
2024-03-27 13:06:24 +0530

    added some css changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -68,7 +68,7 @@ export default function Header() {
             // <Link href="/login">
             <button
               type="button"
-              className="text-sm text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg hover:underline underline-offset-4 px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-sm text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg hover:underline underline-offset-4 px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2"
               onClick={() => signIn("google")}
             >
               <span>Login</span>
