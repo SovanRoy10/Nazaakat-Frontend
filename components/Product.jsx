532c5c9a82f18979e180d53c4f88700ea852d76e
Sovan Roy <roysovan00@gmail.com>
2024-03-27 19:17:37 +0530

    added some css changes

--- components/Product.jsx
+++ components/Product.jsx
@@ -22,7 +22,7 @@ export default function Product(props) {
           >
             <img
               src={props.images}
-              className=" object-cover hover:scale-110  duration-200 md:h-[300px] object-center md:max-w-[300px] md:min-w-[250px] min-w-[123px]"
+              className=" object-cover hover:scale-110  duration-200 md:h-[300px] object-center md:max-w-[300px] md:min-w-[250px] min-w-[115px]"
               alt="product"
             />
           </div>
