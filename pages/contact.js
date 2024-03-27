46063e912140d9ffe7aed9f54167a04f32cc8880
Sovan Roy <roysovan00@gmail.com>
2024-03-27 19:43:55 +0530

    added some css changes

--- pages/contact.js
+++ pages/contact.js
@@ -91,7 +91,7 @@ export default function Contact() {
 
       <section className="text-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <HeadingText text="Contact us" width="24" />
+          <HeadingText text="Contact us" width="20" />
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
             Got a technical issue? Want to send feedback about a beta feature?
             Need details about our Business plan? Let us know.
