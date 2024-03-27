b1f3d14cb37eb17887949a740b0bca9f3517f809
Sovan Roy <roysovan00@gmail.com>
2024-03-27 19:12:00 +0530

    added some css changes

--- components/Products.jsx
+++ components/Products.jsx
@@ -13,7 +13,7 @@ export default function Products({ products }) {
           alt="leftFooter"
           className="lg:w-48  drop-shadow-lg w-24"
         />
-        <h1 className="transparentHeading  md:text-5xl hover:text-[#212121] drop-shadow-lg text-[27px]">
+        <h1 className="transparentHeading  md:text-5xl hover:text-[#212121] drop-shadow-lg text-[25px]">
           Our Best Sellers
         </h1>
         <img
